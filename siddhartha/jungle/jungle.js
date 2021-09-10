ec7648dd643aeae731f2c9a2883a7efe694f70b2
Siddhartha Biswas <siddhartha@siddharthas-mbp.lan>
2021-09-09 20:52:59 -0700

    Moved code

--- siddhartha/jungle/jungle.js
+++ siddhartha/jungle/jungle.js
@@ -12,144 +12,32 @@ function loadPNGs() {
 		img_1 = new Image()
 		img_1.src = 'png/b' + b_i + '.png'
 		animals_1[b_i] = img_1
-	}	
+	}
 }
 
 function setPieces() {
-	pieces = {
-		"0_0": {
-			"player": 0,
-			"animal": 7
-		},
-		"0_6": {
-			"player": 0,
-			"animal": 6
-		},
-		"1_1": {
-			"player": 0,
-			"animal": 4
-		},
-		"1_5": {
-			"player": 0,
-			"animal": 2
-		},
-		"2_0": {
-			"player": 0,
-			"animal": 1
-		},
-		"2_2": {
-			"player": 0,
-			"animal": 5
-		},
-		"2_4": {
-			"player": 0,
-			"animal": 3
-		},
-		"2_6": {
-			"player": 0,
-			"animal": 8
-		},
-		"8_6": {
-			"player": 1,
-			"animal": 7
-		},
-		"8_0": {
-			"player": 1,
-			"animal": 6
-		},
-		"7_5": {
-			"player": 1,
-			"animal": 4
-		},
-		"7_1": {
-			"player": 1,
-			"animal": 2
-		},
-		"6_6": {
-			"player": 1,
-			"animal": 1
-		},
-		"6_4": {
-			"player": 1,
-			"animal": 5
-		},
-		"6_2": {
-			"player": 1,
-			"animal": 3
-		},
-		"6_0": {
-			"player": 1,
-			"animal": 8
-		}
-	};
+	pieces = JSON.parse(JSON.stringify(piece_setup))
 	is_first_click = true
 	turn = 1
 	has_won = false
 }
 
 window.onload = function() {
-	canvas = document.getElementById("drawingCanvas");
-	context = canvas.getContext("2d");
+	canvas = document.getElementById("drawingCanvas")
+	context = canvas.getContext("2d")
 	print_rules()
 
-	canvas.onmouseup = canvasClick;
+	canvas.onmouseup = canvasClick
 
 	setPieces()
-
-	water = [
-		[3, 1],
-		[4, 1],
-		[5, 1],
-		[3, 2],
-		[4, 2],
-		[5, 2],
-		[3, 4],
-		[4, 4],
-		[5, 4],
-		[3, 5],
-		[4, 5],
-		[5, 5]
-	]
-	traps = [
-		[7, 3],
-		[8, 2],
-		[8, 4],
-		[0, 2],
-		[1, 3],
-		[0, 4]
-	]
-	den = [
-		[0, 3],
-		[8, 3]
-	]
 	drawBoard()
 }
 
 class WaterJump {
 	constructor(destination, water) {
-		this.destination = destination;
-		this.water = water;
+		this.destination = destination
+		this.water = water
 	}
-}
-
-validMoveWater = {
-	"2_1": [new WaterJump("6_1", ["3_1", "4_1", "5_1"])],
-	"2_2": [new WaterJump("6_2", ["3_2", "4_2", "5_2"])],
-	"3_0": [new WaterJump("3_3", ["3_1", "3_2"])],
-	"4_0": [new WaterJump("4_3", ["4_1", "4_2"])],
-	"5_0": [new WaterJump("5_3", ["5_1", "5_2"])],
-	"6_1": [new WaterJump("2_1", ["5_1", "4_1", "3_1"])],
-	"6_2": [new WaterJump("2_2", ["5_2", "4_2", "3_2"])],
-	"3_3": [new WaterJump("3_0", ["3_2", "3_1"]), new WaterJump("3_6", ["3_4", "3_5"])],
-	"4_3": [new WaterJump("4_0", ["4_2", "4_1"]), new WaterJump("4_6", ["4_4", "4_5"])],
-	"5_3": [new WaterJump("5_0", ["5_2", "5_1"]), new WaterJump("5_6", ["5_4", "5_5"])],
-	"2_4": [new WaterJump("6_4", ["3_4", "4_4", "5_4"])],
-	"2_5": [new WaterJump("6_5", ["3_5", "4_5", "5_5"])],
-	"3_6": [new WaterJump("3_3", ["3_5", "3_4"])],
-	"4_6": [new WaterJump("4_3", ["4_5", "4_4"])],
-	"5_6": [new WaterJump("5_3", ["5_5", "5_4"])],
-	"6_4": [new WaterJump("2_4", ["5_4", "4_4", "3_4"])],
-	"6_5": [new WaterJump("2_5", ["5_5", "4_5", "3_5"])]
 }
 
 function canvasClick(e) {
