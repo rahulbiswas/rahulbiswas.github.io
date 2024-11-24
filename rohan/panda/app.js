a63c107474f7c2e9fcb1deea20eda07f77579eaf
rahulbiswas <rahul@cs.stanford.edu>
2024-11-23 17:31:36 -0800

    commit time

--- rohan/panda/app.js
+++ rohan/panda/app.js
@@ -47,8 +47,8 @@ const PlatformGame = () => {
     <div className="w-full max-w-4xl mx-auto p-4">
       <div className="mb-4 text-center">
         <span className="mr-4">Score: {gameState.score}</span>
-        <span>High Score: {gameState.highScore}</span>
-        <span>committed 1728</span>
+        <span className="mr-4">High Score: {gameState.highScore}</span>
+        <span>committed 1731</span>
       </div>
 
       <div
