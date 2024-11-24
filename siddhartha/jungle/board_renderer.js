3b81270c4492377db0207e21bc54f733114d8923
rahulbiswas <rahul@cs.stanford.edu>
2024-11-24 00:14:45 -0800

    Refactor board rendering logic into separate BoardRenderer class

---
+++ siddhartha/jungle/board_renderer.js
@@ -0,0 +1,65 @@
+class BoardRenderer {
+  getLighterShade(baseColor) {
+    switch (baseColor) {
+      case '#4CAF50':
+        return '#81c784'
+      case '#2196F3':
+        return '#64b5f6'
+      case '#FFC107':
+        return '#ffd54f'
+      case '#9C27B0':
+        return '#ce93d8'
+      default:
+        return baseColor
+    }
+  }
+
+  renderPieces(pieces) {
+    return Object.entries(pieces).map(([position, piece]) => {
+      const [x, y] = position.split('_')
+      const translateX = x * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + (BOARD_SQUARE_WIDTH - PIECE_SIZE) / 2
+      const translateY = y * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y + (BOARD_SQUARE_HEIGHT - PIECE_SIZE) / 2
+
+      return React.createElement('g', {
+          key: position,
+          transform: `translate(${translateX}, ${translateY})`
+        },
+        React.createElement('image', {
+          href: `images/${piece.player === 0 ? 'a' : 'b'}piece.svg`,
+          width: PIECE_SIZE,
+          height: PIECE_SIZE
+        }),
+        React.createElement('image', {
+          href: `images/${piece.player === 0 ? 'a' : 'b'}${piece.animal}.svg`,
+          width: PIECE_SIZE,
+          height: PIECE_SIZE
+        })
+      )
+    })
+  }
+
+  renderMoveIndicators(movedPiece) {
+    if (movedPiece[0][0] === '0') return null
+
+    return [
+      React.createElement('rect', {
+        key: 'move1',
+        x: movedPiece[0][0] * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + BOARD_SQUARE_WIDTH - POTENTIAL_MOVE_LENGTH * 1.3,
+        y: movedPiece[0][1] * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y,
+        width: POTENTIAL_MOVE_LENGTH,
+        height: POTENTIAL_MOVE_LENGTH,
+        fill: 'purple'
+      }),
+      React.createElement('rect', {
+        key: 'move2',
+        x: movedPiece[1][0] * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + BOARD_SQUARE_WIDTH - POTENTIAL_MOVE_LENGTH * 1.3,
+        y: movedPiece[1][1] * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y,
+        width: POTENTIAL_MOVE_LENGTH,
+        height: POTENTIAL_MOVE_LENGTH,
+        fill: 'purple'
+      })
+    ]
+  }
+}
+
+window.boardRenderer = new BoardRenderer()
--- siddhartha/jungle/game_board.js
+++ siddhartha/jungle/game_board.js
@@ -1,19 +1,4 @@
 const GameBoard = ({turn, pieces, movedPiece, selectedPiece}) => {
-  const getLighterShade = (baseColor) => {
-    switch (baseColor) {
-      case '#4CAF50':
-        return '#81c784'
-      case '#2196F3':
-        return '#64b5f6'
-      case '#FFC107':
-        return '#ffd54f'
-      case '#9C27B0':
-        return '#ce93d8'
-      default:
-        return baseColor
-    }
-  }
-
   return React.createElement('svg', {viewBox: '0 0 100 67'},
     React.createElement('rect', {
       width: '100',
@@ -41,7 +26,7 @@ const GameBoard = ({turn, pieces, movedPiece, selectedPiece}) => {
           }
 
           const isSelected = selectedPiece === `${col}_${row}`
-          const squareFill = isSelected ? getLighterShade(fill) : fill
+          const squareFill = isSelected ? window.boardRenderer.getLighterShade(fill) : fill
 
           return React.createElement('rect', {
             key: `${col}-${row}`,
@@ -76,45 +61,7 @@ const GameBoard = ({turn, pieces, movedPiece, selectedPiece}) => {
       }, 'BACK')
     ),
 
-    Object.entries(pieces).map(([position, piece]) => {
-      const [x, y] = position.split('_')
-      const translateX = x * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + (BOARD_SQUARE_WIDTH - PIECE_SIZE) / 2
-      const translateY = y * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y + (BOARD_SQUARE_HEIGHT - PIECE_SIZE) / 2
-
-      return React.createElement('g', {
-          key: position,
-          transform: `translate(${translateX}, ${translateY})`
-        },
-        React.createElement('image', {
-          href: `images/${piece.player === 0 ? 'a' : 'b'}piece.svg`,
-          width: PIECE_SIZE,
-          height: PIECE_SIZE
-        }),
-        React.createElement('image', {
-          href: `images/${piece.player === 0 ? 'a' : 'b'}${piece.animal}.svg`,
-          width: PIECE_SIZE,
-          height: PIECE_SIZE
-        })
-      )
-    }),
-
-    movedPiece[0][0] !== '0' && [
-      React.createElement('rect', {
-        key: 'move1',
-        x: movedPiece[0][0] * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + BOARD_SQUARE_WIDTH - POTENTIAL_MOVE_LENGTH * 1.3,
-        y: movedPiece[0][1] * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y,
-        width: POTENTIAL_MOVE_LENGTH,
-        height: POTENTIAL_MOVE_LENGTH,
-        fill: 'purple'
-      }),
-      React.createElement('rect', {
-        key: 'move2',
-        x: movedPiece[1][0] * BOARD_SQUARE_WIDTH + BOARD_UPPER_LEFT_X + BOARD_SQUARE_WIDTH - POTENTIAL_MOVE_LENGTH * 1.3,
-        y: movedPiece[1][1] * BOARD_SQUARE_HEIGHT + BOARD_UPPER_LEFT_Y,
-        width: POTENTIAL_MOVE_LENGTH,
-        height: POTENTIAL_MOVE_LENGTH,
-        fill: 'purple'
-      })
-    ]
+    window.boardRenderer.renderPieces(pieces),
+    window.boardRenderer.renderMoveIndicators(movedPiece)
   )
 }
