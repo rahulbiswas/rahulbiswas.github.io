7afbd0d621b0cf36951bd45db9fdea6113e3a70c
rahulbiswas <rahul@cs.stanford.edu>
2024-11-25 22:28:16 -0800

    Refactor: Extract minimax logic into separate engine class from AI player

--- siddhartha/jungle/ai_player.js
+++ siddhartha/jungle/ai_player.js
@@ -1,71 +1,6 @@
 class AIPlayer {
-  constructor(evaluator) {
-    this.evaluator = evaluator
-  }
-
-  minimax(pieces, depth, isMaximizingPlayer, player) {
-    if (depth === 0 || this.isGameOver(pieces)) {
-      return [null, this.evaluator.evaluatePosition(pieces, player)]
-    }
-
-    const possibleMoves = this.getPossibleMoves(pieces,
-      isMaximizingPlayer ? player : this.getOpponent(player))
-
-    let bestMove = null
-    let bestEval = isMaximizingPlayer ? -Infinity : Infinity
-    const compareFn = isMaximizingPlayer ?
-      (a, b) => a > b :
-      (a, b) => a < b
-
-    for (const move of possibleMoves) {
-      const newPieces = this.makeMove(pieces, move)
-      const [_, ev] = this.minimax(newPieces, depth - 1, !isMaximizingPlayer, player)
-
-      if (compareFn(ev, bestEval)) {
-        bestEval = ev
-        bestMove = move
-      }
-    }
-
-    return [bestMove, bestEval]
-  }
-
-  makeMove(pieces, move) {
-    const newPieces = {...pieces}
-    delete newPieces[move.from]
-    newPieces[move.to] = pieces[move.from]
-    return newPieces
-  }
-
-  getOpponent(player) {
-    return player === PLAYERS.RED ? PLAYERS.YELLOW : PLAYERS.RED
-  }
-
-  isGameOver(pieces) {
-    return window.checkWinCondition(pieces) !== null
-  }
-
-  getPossibleMoves(pieces, player) {
-    let allMoves = []
-
-    Object.entries(pieces).forEach(([fromPos, piece]) => {
-      if (piece.player === player) {
-        const [fromX, fromY] = fromPos.split('_').map(Number)
-
-        for (let toX = 0; toX < BOARD_WIDTH; toX++) {
-          for (let toY = 0; toY < BOARD_HEIGHT; toY++) {
-            if (window.isValidMove(fromX, fromY, toX, toY, pieces, piece)) {
-              allMoves.push({
-                from: fromPos,
-                to: `${toX}_${toY}`
-              })
-            }
-          }
-        }
-      }
-    })
-
-    return allMoves
+  constructor(engine) {
+    this.engine = engine
   }
 
   getAIMove(pieces, player) {
@@ -76,21 +11,21 @@ class AIPlayer {
     
     for (let depth = 1; depth <= 3; depth++) {
       console.log(`Searching to depth ${depth}...`)
-      const [move, score] = this.minimax(pieces, depth, true, player)
+      const [move, score] = this.engine.minimax(pieces, depth, true, player)
       bestMove = move
 
-      if (score >= this.evaluator.WINNING_SCORE) {
+      if (score >= this.engine.evaluator.WINNING_SCORE) {
         console.log(`Found winning move at depth ${depth}`)
         break
       }
     }
 
     const moveScores = []
-    const possibleMoves = this.getPossibleMoves(pieces, player)
+    const possibleMoves = this.engine.getPossibleMoves(pieces, player)
 
     for (const move of possibleMoves) {
-        const newPieces = this.makeMove(pieces, move)
-        const [_, score] = this.minimax(newPieces, 2, false, player)
+        const newPieces = this.engine.makeMove(pieces, move)
+        const [_, score] = this.engine.minimax(newPieces, 2, false, player)
         
         const fromPiece = pieces[move.from]
         moveScores.push({
@@ -115,4 +50,4 @@ class AIPlayer {
   }
 }
 
-window.aiPlayer = new AIPlayer(window.positionEvaluator)
+window.aiPlayer = new AIPlayer(window.minimaxEngine)
