5ed9d1f18eb416b433752bf9ed7c4aca34c59aab
rahulbiswas <rahul@cs.stanford.edu>
2024-11-27 23:52:58 -0800

    Grade percentages; need to update Bio

--- siddhartha/grades/final_exam_config.js
+++ siddhartha/grades/final_exam_config.js
@@ -1,4 +1,8 @@
 const FINAL_EXAM_CONFIG = {
+  'English 3': 10,
+  'AP United States History': 10,
+  'Mandarin 3': 15,
+  'AP Calculus BC': 10,
 }
 
 function calculateRequiredFinalScore(currentGrade, targetGrade, finalWeight) {
