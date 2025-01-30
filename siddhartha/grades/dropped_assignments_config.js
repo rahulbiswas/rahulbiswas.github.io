063b45449ddc9da9e1cb2f47c7f85caec48bd1b3
rahulbiswas <rahul@cs.stanford.edu>
2025-01-29 19:02:35 -0800

    Macro quiz drop

--- siddhartha/grades/dropped_assignments_config.js
+++ siddhartha/grades/dropped_assignments_config.js
@@ -1,7 +1,7 @@
 const isEligibleForDropping = (course, category, assignment) => {
-  // if (course === 'AP Microeconomics' && category === 'Quizzes') {
-  //   return true
-  // }
+  if (course === 'AP Macroeconomics' && category === 'Quizzes') {
+    return true
+  }
   // if (course === 'AP United States History' && (assignment.total === 9 || assignment.total === 10)) {
   //   return true
   // }
