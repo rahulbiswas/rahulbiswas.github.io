b1e4a5cd564eb4c5a82a61cc9845b99ace0f812d
rahulbiswas <rahul@cs.stanford.edu>
2025-03-22 15:54:04 -0700

    done with skeleton

--- rohan/two/game.js
+++ rohan/two/game.js
@@ -1,42 +1,7 @@
-PELLETS = []
 gx = 9
 gy = 10
-gx2 = 18
-gy2 = 18
-gx3 = 1
-gy3 = 18
 score = 0
-px = 1
-py = 1
-moves = 0
-const MAZE0 = [
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-	[1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-	[1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
-	[1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-	[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
-	[1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-	[1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
-	[1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1],
-	[1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
-	[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1],
-	[0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1],
-	[0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-	[0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
-	[0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
-	[0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1],
-	[1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1],
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
-
-
-MAZE = MAZE0
-
-SIZE = MAZE.length
-
+SIZE = 20
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
@@ -52,15 +17,12 @@ function drawBox(color, x, y) {
 }
 
 function drawBoxes() {
-	// Start with an off white board.
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = getColor('background');
 	ctx.fillRect(0, 0, 900, 900)
 
-	// Draw ghost.
 	drawBox(getColor('blinky'), gx, gy)
 
-	// Show move count.
 	ctx.fillStyle = 'white';
 	ctx.font = '24px Arial';
 	ctx.fillText('score ' + score, 0, 20);
@@ -68,12 +30,7 @@ function drawBoxes() {
 
 function getColor(str) {
   return {
-	    'blinky': '#9C2C77',   // ghost - burgundy purple
-	    'pinky': '#FFCF96',    // ghost - peach
-	    'pacman': '#FFDE59',   // golden yellow (same as fire theme)
-	    'inky': '#3FA796',     // ghost - teal
-	    'wall': '#4A0C04',     // charred wood (same as fire theme)
-	    'pellet': '#FFAA00',   // orange flame (same as fire theme)
-	    'background': '#0F0702' // ash black (same as fire theme)
-	  }[str];
+	    'blinky': '#9C2C77',
+			'background': '#050A30'
+  }[str];
 }
