ba84c28486308d4e52ee60ccd6619d3ffe84d254
rahulbiswas <rahul@cs.stanford.edu>
2025-04-01 22:10:28 -0700

    tile flipping

--- rohan/two/game.js
+++ rohan/two/game.js
@@ -5,21 +5,24 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 PAIRS = 6
 pieces = []
+hidden = []
 for (i = 0; i < PAIRS*2; i++) {
 	pieces.push(i % PAIRS)
+	hidden.push(true)
 }
 pieces = shuffleArray(pieces);
 console.log(pieces)
 drawBoxes();
-canvas.addEventListener('click', function() {
-	drawBoxes();
-});
 
 console.log('Can I keep doing the pacman')
 
 function drawBox(color, x, y) {
 	ctx.fillStyle = color;
-	ctx.fillRect(15 + y * 870 / SIZE + 5, 30 + x * 870 / SIZE + 5, 870 / SIZE - 10, 870 / SIZE - 10)
+	ctx.fillRect(20 + y * 870 / SIZE, 35 + x * 870 / SIZE, 870 / SIZE - 10, 870 / SIZE - 10)
+}
+
+function coordinatesToIndex(x, y) {
+	return 3*x+y
 }
 
 function drawBoxes() {
@@ -29,14 +32,14 @@ function drawBoxes() {
 
 	for (x = 0; x < 4; x++) {
 		for (y = 0; y < 3; y++) {
-			// x = 0,1,2,3
-			// y = 0,1,2
-			// q = 0 if x = 0, y = 0
-			// q = 5 if x = 1, y = 1
-			// q = 4 if x = 0, y = 1
-			q = 3*x+y
+			q = coordinatesToIndex(x,y)
 			p = pieces[q]
-			drawBox(getColor('card' + p), x, y)
+			color = getColor('card' + p)
+			isHidden = hidden[q]
+			if (isHidden) {
+				color = getColor('hidden')
+			}
+			drawBox(color, x, y)
 		}
 	}
 
@@ -45,16 +48,45 @@ function drawBoxes() {
 	ctx.fillText('score ' + score, 0, 20);
 }
 
+canvas.addEventListener('click', function(event) {
+  // Get the canvas position
+  const rect = canvas.getBoundingClientRect();
+  
+  // Calculate mouse position relative to the canvas
+  const px = event.clientX - rect.left;
+  const py = event.clientY - rect.top;
+  
+  console.log('Click coordinates:', px, py);
+	
+	x = Math.floor((py - 35) * SIZE / 870);
+	y = Math.floor((px - 20) * SIZE / 870);
+
+	if ((x < 0) || (y < 0) || (x > 3) || (y > 2)) {
+		return
+	}
+
+	q = coordinatesToIndex(x,y)
+	oldValue = hidden[q]
+	newValue = !oldValue
+	hidden[q] = newValue
+
+  console.log('Click coordinates:', x, y);
+	console.log('Hidden: ' + hidden)
+	
+	drawBoxes()
+});
+
 function getColor(str) {
   return {
 	  'blinky': '#9C2C77',
 		'background': '#050A30',
-		'card0': '#e4ff1a',
-		'card1': '#e8aa14',
-		'card2': '#f21b3f',
-		'card3': '#00bbf9',
-		'card4': '#00f5d4',
-		'card5': '#ffc07f',
+		'card0': '#f00',
+		'card1': '#0f0',
+		'card2': '#00f',
+		'card3': '#ff0',
+		'card4': '#f0f',
+		'card5': '#0ff',
+		'hidden': '#333'
   }[str];
 }
 
