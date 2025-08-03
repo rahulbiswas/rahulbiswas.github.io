31ea845276fea68adfe5ef1510bf5d99d311f899
rahulbiswas <rahul@cs.stanford.edu>
2025-08-03 15:15:38 -0700

    fish max

--- rohan/seaweed/board.js
+++ rohan/seaweed/board.js
@@ -18,8 +18,8 @@ function createSeaweeds() {
     let newSeaweedLocations = [];
     let numSeaweeds = 1 + Math.floor(Math.random() * 19);
     for (let seaweedIndex = 0; seaweedIndex < numSeaweeds; seaweedIndex++) {
-        let seaweedX = Math.floor(Math.random() * 9);
-        let seaweedY = Math.floor(Math.random() * 9);
+        let seaweedX = Math.floor(Math.random() * (SIZE - 1));
+        let seaweedY = Math.floor(Math.random() * (SIZE - 1));
         newSeaweedLocations.push({x: seaweedX, y: seaweedY});
     }
     return newSeaweedLocations;
@@ -27,10 +27,9 @@ function createSeaweeds() {
 
 function createBoard(fishLocations, seaweedLocations) {
     const board = []
-    const SIDE = 10
-    for (let i = 0; i < SIDE; i++) {
+    for (let i = 0; i < SIZE; i++) {
         const row = []
-        for (let j = 0; j < SIDE; j++) {
+        for (let j = 0; j < SIZE; j++) {
             row.push('x')
         }
         board.push(row)
@@ -61,14 +60,14 @@ function createBoard(fishLocations, seaweedLocations) {
         }
 
         // right
-        for (let y = fy + 1; y <= 9; y++) {
+        for (let y = fy + 1; y < SIZE; y++) {
             let c = board[fx][y]
             if ((c === 's') || (c === 'f')) break
             board[fx][y] = '.'
         }
 
         // down
-        for (let x = fx + 1; x <= 9; x++) {
+        for (let x = fx + 1; x < SIZE; x++) {
             let c = board[x][fy]
             if ((c === 's') || (c === 'f')) break
             board[x][fy] = '.'
@@ -77,7 +76,7 @@ function createBoard(fishLocations, seaweedLocations) {
     return board;
 }
 
-function toggleFish(x, y, fishLocations, seaweedLocations) {
+function toggleFish(x, y, fishLocations, seaweedLocations, targetMinFish) {
     for (let seaweedLocation of seaweedLocations) {
         if (seaweedLocation.x === x) {
             if (seaweedLocation.y === y) {
@@ -96,7 +95,10 @@ function toggleFish(x, y, fishLocations, seaweedLocations) {
         
     if (fishExists) {
         return fishLocations.filter(fish => !(fish.x === x && fish.y === y));
-    } else {    
+    } else {
+        if (targetMinFish !== -1 && fishLocations.length >= targetMinFish) {
+            return fishLocations;
+        }
         return [...fishLocations, {x: x, y: y}];
     }
 }
